ff8d935b23a78ab8e2484e398cddba79be682688
wootaepark <pakdal99@gmail.com>
2026-08-05 21:04:51 +0900

    feat: Swagger 환경 세팅

---
+++ src/main/java/com/bosalpim/compozi_ai/config/SwaggerConfig.java
@@ -0,0 +1,25 @@
+package com.bosalpim.compozi_ai.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+    @Bean
+    public OpenAPI openAPI() {
+        return new OpenAPI()
+                .components(new Components())
+                .info(apiInfo());
+    }
+
+    private Info apiInfo() {
+        return new Info()
+                .title("compozi-ai api 문서")
+                .description("compozi-ai api 문서 입니다.")
+                .version("1.0.0");
+
+    }
+}
