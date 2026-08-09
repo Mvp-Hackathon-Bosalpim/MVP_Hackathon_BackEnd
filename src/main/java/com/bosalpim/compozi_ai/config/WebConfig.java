cda8606a041b0608da804bdbf1b6ad6487777390
wootaepark <pakdal99@gmail.com>
2026-08-09 21:04:55 +0900

    fix: PATCH cors origin

--- src/main/java/com/bosalpim/compozi_ai/config/WebConfig.java
+++ src/main/java/com/bosalpim/compozi_ai/config/WebConfig.java
@@ -14,7 +14,7 @@ public class WebConfig implements WebMvcConfigurer {
                         "http://bosalpim.cloud",
                         "http://localhost:8080",
                         "http://localhost:3000")
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH")
                 .allowedHeaders("*");
     }
 }
